f946ec0b97d24a89353d5d47f3deee0b54254772
Adam Wright <adam@adamjwright.com>
2020-06-27 22:38:13 -0700

    update gitignore

--- Project-1/Simple_server.py
+++ Project-1/Simple_server.py
@@ -1,4 +1,9 @@
 """
-
-
+Author:         Adam Wright
+Email:          wrighada@oregonstate.edu
+Date:           6/27/2020 
+Description:    Project 1 from OSU CS-372 Summer 2020. The project uses the
+                Python socket API to make a GET request and then uses it to
+                create a very simple web server.
 """
+
