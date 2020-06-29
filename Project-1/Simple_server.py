250f2fe109eba5261339e9cff0c2bbdea5a80846
Adam Wright <adam@adamjwright.com>
2020-06-28 20:36:47 -0700

    make 1st get request

--- Project-1/Simple_server.py
+++ Project-1/Simple_server.py
@@ -7,3 +7,34 @@ Description:    Project 1 from OSU CS-372 Summer 2020. The project uses the
                 create a very simple web server.
 """
 
+import socket
+
+# Global targets for the Client requests 
+TARGET_PORT = 80
+TARGET_URL = "gaia.cs.umass.edu"
+TARGET_HOST = "HTTP/1.1\r\nHost:gaia.cs.umass.edu\r\n\r\n"
+
+
+# Create a new socket instance and connect to required URL
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TARGET_URL, TARGET_PORT))
+
+
+# Form and make the 1st GET request
+request ="GET /wireshark-labs/INTRO-wireshark-file1.html " + TARGET_HOST
+s.send(request.encode())
+
+# Print out the connection information and get request response text
+print("**********************\n")
+print(f"Request:{request}")
+
+# Recieve the reply and print to console
+response = s.recv(2048)  
+print(response.decode())
+
+# Form and make the 2nd GET request
+request_2 = "GET /wireshark-labs/HTTP-wireshark-file3.html " + TARGET_HOST
+s.send(request.encode())
+
+# Close the socket
+s.close()
